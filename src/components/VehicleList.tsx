a61cbd3c10e625c8cb9d44e45906b3cdb36e8321
Tonnwp <nameton2107@gmail.com>
2024-05-10 15:07:12 +0700

    create  service/api/vehicles.ts and move function fetchget

--- src/components/VehicleList.tsx
+++ src/components/VehicleList.tsx
@@ -1,32 +1,22 @@
 import React, { useEffect, useState } from "react";
-// application
-import { collection, getDocs } from "firebase/firestore";
-import Ivehicle from "~/interfaces/vehicle";
-import db from "~/services/firebase";
+import Vehicles from "~/services/api/vehicles"
 
 function VehicleList() {
-    const [todos, setTodos] = useState<Ivehicle[]>([]);
-
-    const fetchPost = async () => {
-        const querySnapshot = await getDocs(collection(db, "vehicles"));
-        const newData: Ivehicle[] = querySnapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id }));
-        setTodos(newData);
-        console.log("Eit X", todos, newData);
-    };
+    const { vehicles, fetchget } = Vehicles();
 
     useEffect(() => {
-        fetchPost();
+        fetchget();
     }, []);
 
     return (
         <div className="container">
             <h1>Vehicle Data</h1>
-            {todos.map((todo, i) => (
+            {vehicles.map((vehicle, i) => (
                 <div key={i}>
-                    <p>id: {todo.id}</p>
-                    <p>Make: {todo.make}</p>
-                    <p>model: {todo.model}</p>
-                    <p>Year: {todo.year}</p>
+                    <p>id: {vehicle.id}</p>
+                    <p>Make: {vehicle.make}</p>
+                    <p>model: {vehicle.model}</p>
+                    <p>Year: {vehicle.year}</p>
                 </div>
             ))}
         </div>
